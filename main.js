a2a1f674f922ba38dfad1eabb7a38eac2d776d04
BarbatosLopux <sneideresteban@gmail.com>
2023-09-22 13:36:17 -0500

    feat: :tada: Creacion del repositorio Pokemon Api's

---
+++ main.js
@@ -0,0 +1,29 @@
+
+let myPikachu = document.querySelector("#myPikachu");
+
+myPikachu.addEventListener("click", async () => {
+    let res = await (await fetch("https://pokeapi.co/api/v2/pokemon/mewtwo")).json();
+    let img = res.sprites.front_default;
+    // let defaultImg = colocar imagen por defecto
+
+
+    Swal.fire({
+        title: `${res.name}`,
+        text: 'Modal with a custom image.',
+        // imageUrl: `${res.sprites.front_default}`,
+        imageUrl: `${(img) ? img : defaultImg}`,
+        html: `
+            ${res.stats.map(data => `
+                <input 
+                    type="range" 
+                    id="uno" 
+                    value="${data.base_stat}">
+                <label for="uno"> 
+                    ${data.base_stat} 
+                    ${data.stat.name}</label><br>
+                    `).join("")}   
+        `,
+        imageWidth: "80%",
+        imageHeight: "80%",
+    });
+});
