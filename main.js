7d6529ff87b45e58311b02b0796e2b04de10dd67
Kevin Esteban <139156915+BarbatosLopux@users.noreply.github.com>
2023-09-27 07:43:44 -0500

    feat: ✨ Modificacion de codigo e inclusion por cantidad de pokemones

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
+var urlMain = "https://pokeapi.co/api/v2/pokemon/"
+var query = "?offset=500&limit=500"
+let cons = ""
+
+var number = 20;
+
+
+const lectorCantidad = (value)=>{
+    let cant = `?offset=0&limit=${value}`
+    return urlMain+cant;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const pokemonContainer = document.getElementById("pokemon-container");
 
     async function fetchPokemonData(pokemonName) {
         try {
-            const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
-            const res = await fetch(apiUrl);
-            if (!res.ok) {
-                throw new Error("Network response was not ok");
+            if(pokemonName === undefined){
+                return {}
+            }else{
+                const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+                console.log(apiUrl);
+                const res = await fetch(apiUrl);
+                if (!res.ok) {
+                    throw new Error("Network response was not ok");
+                }
+    
+                const data = await res.json();
+                return data;
             }
-            const data = await res.json();
-            return data;
         } catch (error) {
             console.error(error);
             Swal.fire({
@@ -20,36 +38,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    async function saveStats(pokemonName, newHP) {
-        try {
-            const mockApiUrl = `https://6513a6898e505cebc2ea0473.mockapi.io/:endpoint/${pokemonName}`;
-            await fetch(mockApiUrl, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ hp: newHP })
-            });
-            Swal.fire({
-                icon: 'success',
-                title: 'Success',
-                text: 'Statistics saved successfully!',
-            });
-        } catch (error) {
-            console.error(error);
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Something went wrong!',
-            });
-        }
-    }
-    let numberOfPokemonToShow = 10;
-    const maxPokemonCount = 400;
-const allPokemonNames = [];
-for (let i = 1; i <= maxPokemonCount; i++) {
-    allPokemonNames.push(`pokemon/${i}`);
-}
+
 function createPokemonCard(pokemonName, data) {
     const card = document.createElement("div");
     card.classList.add("pokemon");
@@ -57,17 +46,17 @@ function createPokemonCard(pokemonName, data) {
         <h3>${pokemonName}</h3>
         <img src="${data.sprites.front_default}" alt="${pokemonName}">
         <p><label>HP:</label></p>
-        <p><progress class="hp-progress" max="100" value="${(data.stats[0].base_stat / 255) * 100}"></progress></p>
+        <p><progress class="hp-progress" max="50" value="${(data.stats[0].base_stat / 255) * 100}"></progress></p>
         <p><label>Attack:</label></p>
-        <p><progress class="attack-progress" max="100" value="${(data.stats[1].base_stat / 255) * 100}"></progress></p>
+        <p><progress class="attack-progress" max="50" value="${(data.stats[1].base_stat / 255) * 100}"></progress></p>
         <p><label>Defense:</label></p>
-        <p><progress class="defense-progress" max="100" value="${(data.stats[2].base_stat / 255) * 100}"></progress></p>
+        <p><progress class="defense-progress" max="50" value="${(data.stats[2].base_stat / 255) * 100}"></progress></p>
         <p><label>Special Attack:</label></p>
-        <p><progress class="special-attack-progress" max="100" value="${(data.stats[3].base_stat / 255) * 100}"></progress></p>
+        <p><progress class="special-attack-progress" max="50" value="${(data.stats[3].base_stat / 255) * 100}"></progress></p>
         <p><label>Special Defense:</label></p>
-        <p><progress class="special-defense-progress" max="100" value="${(data.stats[4].base_stat / 255) * 100}"></progress></p>
+        <p><progress class="special-defense-progress" max="50" value="${(data.stats[4].base_stat / 255) * 100}"></progress></p>
         <p><label>Speed:</label></p>
-        <p><progress class="speed-progress" max="100" value="${(data.stats[5].base_stat / 255) * 100}"></progress></p>
+        <p><progress class="speed-progress" max="50" value="${(data.stats[5].base_stat / 255) * 100}"></progress></p>
         <p><button class="save-btn">Save</button></p>
     `;
 
@@ -79,34 +68,32 @@ function createPokemonCard(pokemonName, data) {
     return card;
 }
 
+    const pokemones_arra =async () =>{
+        let res = await (await fetch(lectorCantidad(100))).json();
+        console.log(res)
+        let arrPokemones = res.results.map( ele =>{
+            return {
+                name: ele.name,
+                url: ele.url
+            }
+        });
+        console.log(arrPokemones)
+        return arrPokemones;
+    }
 
+    (async()=>{
+        let arr = await pokemones_arra();
+        arr.forEach(async (ele) =>{
+            let dataPoke = await fetchPokemonData(ele.name);
 
+            const card = createPokemonCard(ele.name, dataPoke);
 
-    const pokemonNames = [
-        "pikachu",
-        "charizard",
-        "charmander",
-        "mewtwo",
-        "ditto",
-        "bulbasaur",
-        "ivysaur",
-        "venusaur" ,
-        "mew",
-        "dragonite",
-        "dragonair",
-        "dratini",
-        "moltres",
-        "zapdos",
-        "articuno",
-        "snorlax",
-        "aerodactyl",
-        "kabutops",
-
-    ];
-
-    pokemonNames.forEach(async (pokemonName) => {
-        const data = await fetchPokemonData(pokemonName);
-        const card = createPokemonCard(pokemonName, data);
-        pokemonContainer.appendChild(card);
-    });
+            pokemonContainer.insertAdjacentElement("beforeend",card)
+    
+            const data = await fetchPokemonData();
+            console.log(data);
+            
+        })
+    })()
 });
+
